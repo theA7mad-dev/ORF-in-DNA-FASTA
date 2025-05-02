aba395eb34dee74b49dad1c2948a2f948c107eb0
Ahmed_e_Khair <ahmad.eiad@gmail.com>
2025-05-03 02:15:21 +0300

    Update ORF-in-DNA-FASTA.py

--- ORF-in-DNA-FASTA.py
+++ ORF-in-DNA-FASTA.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 The goal of this file is to go through a FASTA file
-a check for how many records are in it, what is the
+a check for how many records are in it, what are the
 longest and shortest records. How many open reading frames
 are in each record and for each frame (1,2, or 3)
 """
